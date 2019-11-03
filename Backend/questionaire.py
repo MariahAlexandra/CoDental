cb9b6743dc85e8c685aac9cad89c877f6248b9cb
harbm17 <miriam.harb2017@gmail.com>
2019-11-03 00:36:01 -0400

    adding final functions

--- Backend/questionaire.py
+++ Backend/questionaire.py
@@ -1,39 +1,34 @@
-def q1(info):
+#CATEGORY: Personal Info
+  # Take user input of Name, Age and Gender
 
+#CATEGORY: Symptoms
+def q1(pain):
     # Question 1: Do you feel pain to any of your teeth?
-    if info["pain"] == True:
-        if info["fever"] == True:
+    if pain == True:
             return "https://www.mayoclinic.org/diseases-conditions/tooth-abscess/symptoms-causes/syc-20350901"
-        else:
-            return "Go see a Dentist."
+    else:
+            return -1
 
-def q2(info):
+#CATEGORY: Habits
+def q2(bleeding):
     # Question 2: Do you gums bleed while brushing or flossing?
-    if info["bleeding"] == True:
-        if info["brushing too hard"] == True:
-            return "https://www.everydayhealth.com/hs/sensitive-teeth/brush-teeth-too-hard/"
-        if info["flossing"] == False:
-            return "https://www.medicalnewstoday.com/articles/241721.php#complications"
-        else:
-            return -1
+    if bleeding == True:
+        return "You are brushiong too hard. Take a look at this resouce: https://www.everydayhealth.com/hs/sensitive-teeth/brush-teeth-too-hard/"
+    else:    
+        return -1
 
-def q3(info):
-    # Question 3: Do you use tobacco?
-        if info["tobacco"] == True:
-            return "Gum Disease: https://www.webmd.com/oral-health/features/gums-problems-gingivitis#1"
-        else:
-            return -1
-        
+#CATEGORY: Medicines
+def q4(meds):
     # Question 4: Are you taking any medicine(s) including non-prescription medicine?
-        if info["medicine"] == True:
-            return "https://www.guardiandirect.com/resources/articles/5-medications-may-be-causing-your-teeth-decay"
-        else:
-            return -1
-    # Question 5: Do you or have you used illegal substances?
-        if info["illegal substances"] == True:
-            return "https://www.webmd.com/oral-health/drug-abuse-mouth#1"
-        else:
-            return -1
+    if meds == True:
+        return "https://www.guardiandirect.com/resources/articles/5-medications-may-be-causing-your-teeth-decay"
+    else:
+        return -1
 
-        
-        
+#CATEGORY: Dental Insurance
+def q5(insurance):
+    #Question 5: Do you have Dental Insurance?
+    if insurance == False:
+        return "Dental Care Alternatives: https://www.dentalplans.com/dental-information/dental-insurance/dental-care-for-the-uninsured"
+    else:
+        return -1
