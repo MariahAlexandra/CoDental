f4cb71f7ebdea7fd66a43e91d191ed7f56aa0361
jackiekcruz <jackie.k.cruz@gmail.com>
2019-11-03 01:39:52 -0400

    added routes

--- Backend/app.py
+++ Backend/app.py
@@ -1,2 +1,20 @@
 from questionaire import *
 
+info ={}
+info['Q1'] = True if raw_input("Do you gums bleed while brushing or flossing?") == 'Y' else False 
+info['Q2'] = True if raw_input("Are your teeth sensitive to hot or cold liquids/food?") == 'Y' else False 
+info['Q3'] = True if raw_input("Are your teeth sensitive to sweet or sour liquids/foods?") == 'Y' else False 
+info['Q4'] = True if raw_input("Do you feel pain to any of your teeth?") == 'Y' else False 
+info['Q5'] = True if raw_input("Do you use tobacco?") == 'Y' else False 
+info['Q6'] = True if raw_input("Do you or have you used illegal substances?") == 'Y' else False 
+info['Q7'] = True if raw_input("Are you taking any medicine(s) including non-prescription medicine?") == 'Y' else False 
+info['Q8'] = True if raw_input("Do you have any sores or lumps in or near your mouth?") == 'Y' else False 
+info['Q10'] = True if raw_input("Do you have frequent headaches?") == 'Y' else False 
+info['Q11'] = True if raw_input("Do you clench or grind your teeth?") == 'Y' else False 
+info['Q12'] = True if raw_input("Do you bite your lips or cheeks frequently?") == 'Y' else False 
+info['Q13'] = True if raw_input("Have you noticed any loosening of your teeth?") == 'Y' else False 
+info['Q14'] = True if raw_input("Have you had any difficult extractions recently?") == 'Y' else False 
+info['Q15'] = True if raw_input("Do you wear dentures or partials?") == 'Y' else False 
+info['Q16'] = True if raw_input("Have you ever received oral hygiene instructions regarding the care of your teeth and gums?") == 'Y' else False 
+
+
--- Backend/app2.py
+++ Backend/app2.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template, Response, redirect, url_for
 from project import app
+from questionaire import *
 # from project.services.EventService import *
 # from project.services.VenueService import *
 
@@ -12,6 +13,47 @@ from project import app
 @app.route('/')
 def rootPage():
     return 'hello, world'
+
+@app.route('/send_data', methods=['POST'])
+def send_data():
+    points = 0 
+    responses = []
+    pain = True if request.form['pain'] == 'True' else False 
+    pain = q1(pain)
+    if pain != -1:
+        points += 3
+        responses.append(pain) 
+
+    bleeding = True if request.form['bleeding'] == 'True' else False
+    bleeding = q2(bleeding)
+    if bleeding != -1:
+        points += 2 
+        responses.append(bleeding)
+    
+    meds = True if request.form['meds'] == 'True' else False
+    meds = q4(meds)
+    if meds != -1:
+        points += 1
+        responses.append(meds)
+    
+    insurance = True if request.form['insurance'] == 'True' else False 
+    insurance = q5(insurance)
+    if insurance != -1:
+        points += 4
+        responses.append(insurance)
+
+    severity = ''
+
+    if points <= 2:
+        severity = 'low'
+    if points <= 6 and points > 2:
+        severity = 'medium'
+    if points > 6:
+        severity = 'high'
+    
+    res = {"severity":severity, 'responses':responses}
+    return res
+
 
 if __name__ == "__main__":
 	app.run(debug=False)
